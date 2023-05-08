928baccc0bdfa8d49ad6590a307aaa6f812ce23b
AkaiNoval <127651185+AkaiNoval@users.noreply.github.com>
2023-05-08 23:24:34 +0700

    = Modify Frequency

--- Assets/RadioFrequencyVisualizer.cs
+++ Assets/RadioFrequencyVisualizer.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(LineRenderer))]
 public class RadioFrequencyVisualizer : MonoBehaviour
@@ -12,7 +13,9 @@ public class RadioFrequencyVisualizer : MonoBehaviour
     [Range(-3, 3)]
     public int phase;
     [SerializeField] bool isRandom;
-    private LineRenderer lineRenderer;
+    LineRenderer lineRenderer;
+    [SerializeField] bool startCoroutine;
+    private Coroutine randomizeCoroutine;
 
     void Start()
     {
@@ -20,7 +23,15 @@ public class RadioFrequencyVisualizer : MonoBehaviour
         lineRenderer.positionCount = numberOfPoints;
         if (isRandom)
         {
-            RandomizeFrequencyAndPhase();
+            Randomize();
+        }
+    }
+    IEnumerator RandomizeEvery10Seconds()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(10.0f);
+            Randomize();
         }
     }
     private void Update()
@@ -36,10 +47,31 @@ public class RadioFrequencyVisualizer : MonoBehaviour
 
             x += deltaX;
         }
+        if (startCoroutine && randomizeCoroutine == null)
+        {
+            randomizeCoroutine = StartCoroutine(RandomizeEvery10Seconds());
+        }
+        else if (!startCoroutine && randomizeCoroutine != null)
+        {
+            StopCoroutine(randomizeCoroutine);
+            randomizeCoroutine = null;
+        }
     }
-    private void RandomizeFrequencyAndPhase()
+    private void Randomize()
     {
+        //amplitude = Mathf.Round(Random.Range(0.5f, 1.0f) * 10.0f) / 10.0f;
         frequency = Random.Range(-3, 4);
         phase = Random.Range(-3, 4);
     }
 }
+/*
+    The * 10 and / 10 are used to move the decimal point one digit to the right and then back to the left again, 
+    effectively rounding the value to one digit after the decimal point.
+    Here's how it works:
+       Random.Range(0.0f, 1.0f) generates a random float value between 0 and 1 with many digits after the decimal point, such as 0.7632845.
+       Multiplying this value by 10 results in a larger number with the same number of digits after the decimal point, such as 7.632845.
+       Rounding this value to the nearest integer using Mathf.Round results in either 7 or 8, depending on whether the original value was closer to 7 or 8.
+       Dividing this rounded value by 10 moves the decimal point one digit to the left, resulting in a final value with one digit after the decimal point, such as 0.7 or 0.8.
+    The * 10 and / 10 operations are necessary to ensure that the rounding is done correctly. If we omitted these operations, the rounding would be incorrect because Mathf.
+    Round rounds to the nearest integer, not the nearest tenth.
+*/
