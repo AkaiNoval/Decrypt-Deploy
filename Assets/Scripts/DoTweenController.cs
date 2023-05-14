f42491263ec8d4105b1c078b6866f0b347332add
AkaiNoval <127651185+AkaiNoval@users.noreply.github.com>
2023-05-15 01:18:57 +0700

    Update DoTweenController.cs

--- Assets/Scripts/DoTweenController.cs
+++ Assets/Scripts/DoTweenController.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DoTweenController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] SpriteRenderer material;
+    [SerializeField] float duration;
+    [SerializeField] Ease ease;
+    [SerializeField] GameObject diamond;
+
     void Start()
     {
-        
+       material= GetComponent<SpriteRenderer>();
+       transform.DOMove(new Vector3(5, 0, 0), duration, false)
+             .SetEase(ease)
+             .SetLoops(-1,LoopType.Yoyo);
+       material.DOColor(Color.green, duration);
+       diamond.transform.DORotate(new Vector3(0, 360, 0), duration*0.5f,RotateMode.FastBeyond360)
+            .SetEase(ease)
+            .SetLoops(-1,LoopType.Restart);
+       diamond.transform.DOLocalMove(new Vector3(0, -3,0),duration)
+            .SetEase(ease)
+            .SetLoops(-1, LoopType.Yoyo);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
